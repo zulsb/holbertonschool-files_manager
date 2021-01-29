5b18ec1bd369c5e484f47fe030fac68f2baf6127
zulsb <zulsb2093@gmail.com>
2021-01-29 03:33:27 -0500

    Redis utils [Fix semicolons]

--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,33 @@
-import redis from 'redis'
-import { promisify } from 'util'
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-  constructor () {
-    this.client = redis.createClient()
-    this.getAsync = promisify(this.client.get).bind(this.client)
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
     this.client.on('error', (error) => {
-      console.log(`Error connecting to server: ${error.message}`)
-    })
+      console.log(`Error connecting to server: ${error.message}`);
+    });
   }
 
-  isAlive () {
-    return this.client.connected
+  isAlive() {
+    return this.client.connected;
   }
 
-  async get (key) {
-    const v = await this.getAsync(key)
-    return v
+  async get(key) {
+    const v = await this.getAsync(key);
+    return v;
   }
 
-  async set (key, value, duration) {
-    this.client.setex(key, duration, value)
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
   }
 
-  async del (key) {
-    this.client.del(key)
+  async del(key) {
+    this.client.del(key);
   }
 }
 
-const redisClient = new RedisClient()
+const redisClient = new RedisClient();
 
-export default redisClient
+export default redisClient;
