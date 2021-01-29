d330fc10d9e10a76ee7db705aa4076bb192b5799
zulsb <zulsb2093@gmail.com>
2021-01-29 00:56:57 -0500

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+import {promisify} from 'util';
+
+export default class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (error) => {
+      console.log(`Error connecting to server: ${error.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const v = await this.getAsync(key);
+    return v;
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
